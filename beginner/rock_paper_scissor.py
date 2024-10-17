b6553b8bf5dec6f32b44e9542409c3871bedb29f
LE LONG THANH <64647578+lelongthanh@users.noreply.github.com>
2024-10-17 22:24:23 +0700

    modified code

--- beginner/rock_paper_scissor.py
+++ beginner/rock_paper_scissor.py
@@ -1,43 +1,39 @@
 import random
 
-AI = ['paper', 'rock', 'scissor']
-AI_random = random.choice(AI)
+options = ['paper', 'rock', 'scissor']
+playing = True
 
-while True:
+while playing:
     print('===Welcome to the game===')
 
     player = input("Enter (paper/rock/scissor): ")
+    AI = random.choice(options)
 
-    if (player=='paper' and AI_random=='paper') or (player=='rock' and AI_random=='rock') or (player=='scissor' and AI_random=='scissor'):
-        print(f"PlayervsAI: {player} = {AI_random}. We\'re all good")
-        break
+    while True:
+        if (player==AI):
+            print(f"PlayervsAI: {player} = {AI}. We\'re all good")
+            break
+            
+        elif (player!=AI):
+            if (player=='paper') and (AI=='rock'):
+                print(f"PlayervsAI: {player} > {AI}. YOU WIN!!")
+            elif (player=='rock') and (AI=='scissor'):
+                print(f"PlayervsAI: {player} > {AI}. YOU WIN!!")
+            elif (player=='scissor') and (AI=='paper'):
+                print(f"PlayervsAI: {player} > {AI}. YOU WIN!!")
+            else:
+                print(f"PlayervsAI: {player} < {AI}. YOU LOSE!!")
+            break
+                          
+        else:
+            print("Invalid value")
+            break
 
-    elif (player=='paper') and (player!=AI_random):
-        if (player=='paper') and (AI_random=='rock'):
-            print(f"layervsAI: {player} > {AI_random}. Okay you WIN!!!")
-            break
-        elif (player=='paper') and (AI_random=='scissor'):
-            print(f"layervsAI: {player} < {AI_random}. OOps wanna try again?!")
-            break
+    tryAgain = input("Try again? (y/n): ")
+    if tryAgain.lower() == 'n':
+        playing = False
+print("Thanks for playing")
 
-    elif (player=='rock') and (player!=AI_random):
-        if (player=='rock') and (AI_random=='scissor'):
-            print(f"layervsAI: {player} > {AI_random}. Okay you WIN!!!")
-            break
-        elif (player=='rock') and (AI_random=='paper'):
-            print(f"layervsAI: {player} < {AI_random}. OOps wanna try again?!")
-            break
-
-    elif (player=='scissor') and (player!=AI_random):
-        if (player=='scissor') and (AI_random=='paper'):
-            print(f"layervsAI: {player} > {AI_random}. Okay you WIN!!!")
-            break
-        elif (player=='scissor') and (AI_random=='rock'):
-            print(f"layervsAI: {player} < {AI_random}. OOps wanna try again?!")
-            break
-        
-    else:
-        print("Invalid value")
 
     
   
