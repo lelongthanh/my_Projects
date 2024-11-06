108f6cf43fbdf0557082afb012e0268fd2cfa341
LE LONG THANH <64647578+lelongthanh@users.noreply.github.com>
2024-11-06 22:40:27 +0700

    adding project

---
+++ beginner/encrypt_decrypt.py
@@ -0,0 +1,32 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    for letter in original_text:
+        if encode_or_decode == "decode":
+            shift_amount *= -1
+            
+        # handle number/symbol/space
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet) #keep index position in range of alphabet list
+            output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+
+# loop playing
+is_playing = True
+while is_playing:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    play = input("Do you want to encode or decode again? (Y/N): ").upper()
+    if play == 'Y':
+        is_playing = True
+    else:
+        is_playing = False
+        print("See you again")
